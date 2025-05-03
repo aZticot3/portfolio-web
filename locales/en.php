401a762e39d7aa606468812f612f96228bb65b26
Clément Bontemps <clement.bontemps@etu.unistra.fr>
2025-05-03 16:22:12 +0200

    add lang

---
+++ locales/en.php
@@ -0,0 +1,11 @@
+<?php
+$trad = [
+  "home" => "Home",
+  "projects" => "Projects",
+  "language" => "Language",
+  "search" => "Search",
+  "search_button" => "Search",
+  "all" => "All projects",
+  "load_more" => "Load more projects"
+];
+?>
---
+++ locales/fr.php
@@ -0,0 +1,11 @@
+<?php
+$trad = [
+  "home" => "Accueil",
+  "projects" => "Projets",
+  "language" => "Langue",
+  "search" => "Rechercher",
+  "search_button" => "Rechercher",
+  "all" => "Tout les projets",
+  "load_more" => "Voir plus de projets"
+];
+?>
